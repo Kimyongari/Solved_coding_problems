31ec6639293e52975822a01ef69ff393bda0393c
용가리 <kimybj123@naver.com>
2024-05-21 13:34:18 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,14 @@
+def solution(n, times):
+    start = 1
+    end = max(times) * n
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for i in times:
+            cnt += mid // i
+        if cnt < n:
+            start = mid + 1
+        else:
+            end = mid - 1
+            answer = mid
+    return answer
