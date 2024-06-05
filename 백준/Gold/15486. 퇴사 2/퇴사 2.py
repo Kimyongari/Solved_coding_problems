33633995e30a481ba8cc3ed0874673e7d3dd65db
용가리 <kimybj123@naver.com>
2024-06-05 16:58:17 +0900

    [Gold V] Title: 퇴사 2, Time: 2532 ms, Memory: 251192 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = []
+arr.append((0, 0))
+for _ in range(n):
+    arr.append(tuple(map(int, input().split())))
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    cost, reward = arr[i]
+    dp[i] = max(dp[i], dp[i - 1])
+    fin_date = i + cost - 1
+    if fin_date <= n:
+        dp[fin_date] = max(dp[fin_date], dp[i - 1] + reward)
+
+print(dp[n])
