92c9114cd5a6bd58d74918caa1ceaffe0b7521f0
용가리 <kimybj123@naver.com>
2024-06-09 01:48:32 +0900

    [Silver I] Title: 스티커, Time: 796 ms, Memory: 47968 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,32 +1,20 @@
-import sys
-input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    l = []
+    n = int(input())
+    l.append(list(map(int, input().split())))
+    l.append(list(map(int, input().split())))
+    dp = [[0 for _ in range(n)] for _ in range(2)]
 
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(2)]
-
-    # 2행 DP배열 형성
-    DP = [[0] * N for _ in range(2)]
-
-    # 스티커 길이가 1일 경우
-    DP[0][0] = arr[0][0]
-    DP[1][0] = arr[1][0]
-    if N == 1:
-        print(max(DP[0][0], DP[1][0]))
+    dp[0][0] = l[0][0]
+    dp[1][0] = l[1][0]
+    if n == 1:
+        print(max(dp[0][0], dp[1][0]))
         continue
-
-    # 스티커 길이가 2일 경우
-    DP[0][1] = arr[1][0] + arr[0][1]
-    DP[1][1] = arr[0][0] + arr[1][1]
-    if N == 2:
-        print(max(DP[0][1], DP[1][1]))
-        continue
-
-    # 스티커 길이가 3이상일 경우
-    for i in range(2, N):
-        # 메인 아이디어
-        DP[0][i] = max(DP[1][i - 2], DP[1][i - 1]) + arr[0][i]
-        DP[1][i] = max(DP[0][i - 2], DP[0][i - 1]) + arr[1][i]
-
-    print(max(DP[0][-1], DP[1][-1]))
+    dp[0][1] = l[1][0] + l[0][1]
+    dp[1][1] = l[0][0] + l[1][1]
+    
+    for i in range(2,n):
+        dp[0][i] = max(dp[1][i-2] + l[0][i], dp[1][i-1] + l[0][i])
+        dp[1][i] = max(dp[0][i-2] + l[1][i], dp[0][i-1] + l[1][i])
+    print(max(dp[1][-1], dp[0][-1]))
